53a5d7d1a04d3d9c2eca9b7a5eb3f4601dcd50f1
xueshun <xueshun1211@163.com>
2017-03-14 13:47:15 +0800

    11

--- StringUse/joinerUse/CharsetsUse.java
+++ StringUse/joinerUse/CharsetsUse.java
@@ -1,5 +1,13 @@
 package joinerUse;
 
+/**
+ * 字符集
+ * @author Administrator
+ *
+ */
 public class CharsetsUse {
-
+	public static void main(String[] args) {
+		String str01 = "hello word";
+		str01.getBytes();
+	}
 }
